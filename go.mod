685e533f7b663bad06b296acf108cbf91034ea2f
Josh Mervine <joshua@mervine.net>
2020-03-06 20:57:09 -0800

    Fixing and switching to go mod

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/jmervine/noop-server
+
+go 1.13
+
+require (
+	github.com/jmervine/env v0.0.0-20150903161524-c9fbebdd043f
+	github.com/jmervine/readable v0.0.0-20150925011043-6f6c7afbdd1b
+	github.com/joho/godotenv v1.2.0
+)
--- noop-server.go
+++ noop-server.go
@@ -16,18 +16,16 @@ var (
 )
 
 func init() {
-	port = os.Getenv("PORT")
-	if port == "" {
+    var ok bool
+    if port, ok = os.LookupEnv("PORT"); !ok {
 		port = "3000"
 	}
 
-	addr = os.Getenv("ADDR")
-	if addr == "" {
+	if addr, ok = os.LookupEnv("ADDR"); !ok {
 		addr = "0.0.0.0"
 	}
 
-	app = os.Getenv("APP_NAME")
-	if app == "" {
+	if app, ok = os.LookupEnv("APP_NAME"); !ok {
 		app = "noop-server"
 	}
 
@@ -41,19 +39,22 @@ func init() {
 }
 
 func main() {
+    log.SetFlags(0)
+    log.SetPrefix("at=main ")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		begin := time.Now()
 		status := http.StatusOK
 
 		defer func() {
-			logPrefix := fmt.Sprintf("at=main on=http.HandleFunc method=%s path=%s", r.Method, r.URL.Path)
+			logPrefix := fmt.Sprintf("on=http.HandleFunc method=%s path=%s ", r.Method, r.URL.Path)
+
 			log.Printf("%s status=%d took=%v\n", logPrefix, status, time.Since(begin))
 			if verbose {
-				log.Printf("%s headers:\n%s", r.Header)
+				log.Printf("%s headers:\n%s", logPrefix, r.Header)
 				body, err := ioutil.ReadAll(r.Body)
 				if err == nil {
-					log.Printf("%s body:\n%s", string(body))
+					log.Printf("%s body:\n%s", logPrefix, string(body))
 				}
 			}
 		}()
@@ -71,6 +72,6 @@ func main() {
 		http.Error(w, fmt.Sprintf("%d %s", status, http.StatusText(status)), status)
 	})
 
-	log.Printf("at=main on=startup addr=%s port=%s\n", addr, port)
+	log.Printf("on=startup addr=%s port=%s\n", addr, port)
 	log.Fatal(http.ListenAndServe(addr+":"+port, nil))
 }
